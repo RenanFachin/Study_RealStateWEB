c5998e3d9a83618974ab5ea89833a860b2c7b7af
Renan <renan.fachin@hotmail.com>
2023-02-08 21:49:48 -0300

    add assets

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./index.html"
+  ],
+  theme: {
+    fontFamily: {
+      primary: 'Poppins',
+    },
+    extend: {
+      colors: {
+        primary: '#101828',
+        secondary: '#7F56D9',
+      },
+      boxShadow: {
+        1: '0px 4px 30px rgba(0, 0, 0, 0.08)',
+      },
+    },
+  },
+  plugins: [],
+}
